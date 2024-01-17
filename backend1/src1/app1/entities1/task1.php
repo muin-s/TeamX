f4739a3cf0d7051b2e8d399ec10dfe995c5b7a1d
muin-s <75922386+muin-s@users.noreply.github.com>
2024-01-17 20:40:24 +0530

    Update task1.php

--- backend1/src1/app1/entities1/task1.php
+++ backend1/src1/app1/entities1/task1.php
@@ -1,89 +1,60 @@
 <?php
 
-namespace App\Entities;
+namespace App\Entities1;
 
 use Illuminate\Contracts\Support\Arrayable;
 
-class Task implements Arrayable
+class Task1 implements Arrayable
 {
     protected $id;
     protected $description;
     protected $weightPct;
     protected $performedInJobExperience;
 
-    public function toArray() {
-
-        $output = [];
-
-        foreach ( get_class_vars(get_class($this)) as $propName => $propDefaultValue ) {
-            $output[ $propName ] = $this->{$propName};
-        }
-
-        return $output;
-
+    public function toArray()
+    {
+        return get_object_vars($this);
     }
 
     /**
-     * Get the value of description
-     */ 
+     * Getters and setters for description, weightPct, performedInJobExperience
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
     public function getDescription()
     {
         return $this->description;
     }
 
-    /**
-     * Set the value of description
-     *
-     * @return  self
-     */ 
     public function setDescription($description)
     {
         $this->description = $description;
-
         return $this;
     }
 
-    /**
-     * Get the value of weightPct
-     */ 
     public function getWeightPct()
     {
         return $this->weightPct;
     }
 
-    /**
-     * Set the value of weightPct
-     *
-     * @return  self
-     */ 
     public function setWeightPct($weightPct)
     {
         $this->weightPct = $weightPct;
-
         return $this;
     }
 
-    /**
-     * Get the value of performedInJobExperience
-     */ 
     public function getPerformedInJobExperience()
     {
         return $this->performedInJobExperience;
     }
 
-    /**
-     * Set the value of performedInJobExperience
-     *
-     * @return  self
-     */ 
     public function setPerformedInJobExperience($performedInJobExperience)
     {
         $this->performedInJobExperience = $performedInJobExperience;
-
         return $this;
-    }
-
-    public function getId() {
-        return $this->id;
     }
 }
